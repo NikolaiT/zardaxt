3b699d5c67dd3f751612356972bf31ba8c125f62
Nikolai Tschacher <nikolai.tschacher@informatik.hu-berlin.de>
2021-03-11 17:44:52 +0100

    update auto

--- strip.js
+++ strip.js
@@ -1,11 +1,13 @@
 const fs = require('fs')
 
 let data = JSON.parse(fs.readFileSync('database.json'))
+let newData = [];
 
 for (let entry of data) {
   delete entry.src_ip;
   delete entry.dst_ip;
   delete entry.dst_port;
+  newData.push(entry)
 }
 
-fs.writeFileSync('database.json', JSON.stringify(data, null, 2))
+fs.writeFileSync('database.json', JSON.stringify(newData, null, 2))
