e6f44101ea19e122c95d48eaee0a94556f5007d4
Nikolai Tschacher <nikolai.tschacher@informatik.hu-berlin.de>
2021-11-07 11:17:35 +0100

    update auto

--- restart.sh
+++ restart.sh
@@ -10,8 +10,17 @@ then
 else
   echo tcp_fingerprint not running.
 fi
-  
-nohup pipenv run python tcp_fingerprint.py -i eth0 --classify > log/fp.out 2> log/fp.err < /dev/null &
+
+# I fucking hate Python. It's a utterly broken language and ecosystem. JavaScript is 
+# just so much better. Like I spent just too much time to figure this shit out:
+# https://stackoverflow.com/questions/48990067/how-to-run-a-cron-job-with-pipenv
+
+# `which pipenv`
+# /usr/local/bin/pipenv
+
+PATH=/usr/local/bin:$PATH
+
+nohup pipenv run python tcp_fingerprint.py -i eth0 --classify > log/tcp_fp.out 2> log/tcp_fp.err < /dev/null &
 
 # Write tcp_fingerprint's PID to a file
 echo $! > /var/run/tcp_fingerprint.pid
