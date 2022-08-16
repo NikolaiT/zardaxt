8a2bb9c30b1cc63fc10fef0af12ac36b60d5668d
Nikolai Tschacher <admin@incolumitas.com>
2022-08-16 10:18:46 +0200

    added key os_mismatch which compares the user agent os with the tcp/ip fingerprint os

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import _thread
 import json
 import os
+import re
 import traceback
 from tcpip_fp_logging import log
 from dune_client import incr
@@ -10,6 +11,12 @@ from urllib.parse import urlparse, parse_qs
 API_KEY = os.environ.get('API_KEY', '')
 if not API_KEY:
   raise Exception('InvalidAPIKeyException')
+
+regex = re.compile(r'avg=(\d*\.\d*)')
+def S(string):
+  matches = regex.findall(string)
+  if len(matches) > 0:
+    return float(matches[0])
 
 class MyServer(BaseHTTPRequestHandler):
   def __init__(self, data):
@@ -25,6 +32,9 @@ class MyServer(BaseHTTPRequestHandler):
       ip = self.headers.get('X-Real-IP')
     return ip
 
+  def get_user_agent(self):
+    return self.headers.get('user-agent')
+
   def get_query_arg(self, arg):
     query_components = parse_qs(urlparse(self.path).query)
     arg = query_components.get(arg, None)
@@ -35,6 +45,68 @@ class MyServer(BaseHTTPRequestHandler):
     self.send_header('Access-Control-Allow-Origin', '*')
     BaseHTTPRequestHandler.end_headers(self)
 
+  # infer the base operating system from the user-agent
+  # and then infer the operating system from the TCP/IP 
+  # fingerprint and detect if there is a lie
+  def detect_os_mismatch(self, tcp_ip_fp):
+    user_agent = self.get_user_agent()
+    # get os by tcp ip fingerprint
+    # Linux, macOS or Windows
+    tcpip_os = {
+      'linux': max(S(tcp_ip_fp["avgScoreOsClass"]['Android']), S(tcp_ip_fp["avgScoreOsClass"]['Linux'])),
+      'win': S(tcp_ip_fp["avgScoreOsClass"]['Windows']),
+      'mac': max(S(tcp_ip_fp["avgScoreOsClass"]['iOS']), S(tcp_ip_fp["avgScoreOsClass"]['macOS'])),
+    }
+    # get highest OS from TCP/IP fingerprint
+    highestOS = max(tcpip_os, key=tcpip_os.get)
+
+    userAgentOS = 'win'
+    if 'Linux' in user_agent or 'Android' in user_agent:
+      userAgentOS = 'linux'
+    if 'Mac OS' in user_agent or 'iPhone' in user_agent:
+      userAgentOS = 'mac'
+
+    return highestOS != userAgentOS
+
+  def handle_authenticated_lookup(self, ip):
+    lookup_ip = self.get_query_arg('ip')
+    if lookup_ip:
+      log('Api Key provided. Looking up IP {}'.format(lookup_ip), 'api')
+      res = self.data.get(lookup_ip, None)
+      if res:
+        res['ip'] = ip
+        res['lookup_ip'] = lookup_ip
+        res['vpn_detected'] = False
+        if 'fp' in res and 'tcp_mss' in res['fp']:
+          res['vpn_detected'] = res['fp']['tcp_mss'] in [1240, 1361, 1289]
+        self.wfile.write(bytes(json.dumps(res, indent=2, sort_keys=True), "utf-8"))
+      else:
+        msg = {
+          'lookup_ip': lookup_ip, 
+          'msg': 'no fingerprint for this IP ({} db entries)'.format(len(self.data)),
+        }
+        self.wfile.write(bytes(json.dumps(msg, indent=2, sort_keys=True), "utf-8"))
+    else:
+      self.wfile.write(bytes(json.dumps(self.data, indent=2, sort_keys=True), "utf-8"))
+
+  def handle_lookup_by_client_ip(self, ip):
+    log('No Api Key provided. Looking up client IP {}'.format(ip), 'api')
+    res = self.data.get(ip, None)
+    if res:
+      res['ip'] = ip
+      res['vpn_detected'] = False
+      res['os_mismatch'] = self.detect_os_mismatch(res)
+      if 'fp' in res and 'tcp_mss' in res['fp']:
+        res['vpn_detected'] = res['fp']['tcp_mss'] in [1240, 1361, 1289]
+      self.wfile.write(bytes(json.dumps(res, indent=2, sort_keys=True), "utf-8"))
+    else:
+      msg = {
+        'ip': ip, 
+        'msg': 'no fingerprint for this IP ({} db entries)'.format(len(self.data)),
+      }
+      self.wfile.write(bytes(json.dumps(msg, indent=2, sort_keys=True), "utf-8"))
+
+
   def do_GET(self):
     ip = self.get_ip()
     incr('tcp_ip_fingerprint_public', ip)
@@ -44,45 +116,12 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/json")
         self.end_headers()
-        ip = self.get_ip()
         log('Incoming API request from IP: {} with path: {}'.format(ip, self.path), 'api')
         key = self.get_query_arg('key')
-
         if key and API_KEY == key:
-          lookup_ip = self.get_query_arg('ip')
-          if lookup_ip:
-            log('Api Key provided. Looking up IP {}'.format(lookup_ip), 'api')
-            res = self.data.get(lookup_ip, None)
-            if res:
-              res['ip'] = ip
-              res['lookup_ip'] = lookup_ip
-              res['vpn_detected'] = False
-              if 'fp' in res and 'tcp_mss' in res['fp']:
-                res['vpn_detected'] = res['fp']['tcp_mss'] in [1240, 1361, 1289]
-              self.wfile.write(bytes(json.dumps(res, indent=2, sort_keys=True), "utf-8"))
-            else:
-              msg = {
-                'lookup_ip': lookup_ip, 
-                'msg': 'no fingerprint for this IP ({} db entries)'.format(len(self.data)),
-              }
-              self.wfile.write(bytes(json.dumps(msg, indent=2, sort_keys=True), "utf-8"))
-          else:
-            self.wfile.write(bytes(json.dumps(self.data, indent=2, sort_keys=True), "utf-8"))
+          self.handle_authenticated_lookup(ip)
         else:
-          log('No Api Key provided. Looking up client IP {}'.format(ip), 'api')
-          res = self.data.get(ip, None)
-          if res:
-            res['ip'] = ip
-            res['vpn_detected'] = False
-            if 'fp' in res and 'tcp_mss' in res['fp']:
-              res['vpn_detected'] = res['fp']['tcp_mss'] in [1240, 1361, 1289]
-            self.wfile.write(bytes(json.dumps(res, indent=2, sort_keys=True), "utf-8"))
-          else:
-            msg = {
-              'ip': ip, 
-              'msg': 'no fingerprint for this IP ({} db entries)'.format(len(self.data)),
-            }
-            self.wfile.write(bytes(json.dumps(msg, indent=2, sort_keys=True), "utf-8"))
+          self.handle_lookup_by_client_ip(ip)
       else:
         self.send_response(403)
         self.end_headers()
