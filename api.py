f9df35ac458758ecc4c25d4bdd7597f8f0b2aaba
Nikolai Tschacher <admin@incolumitas.com>
2023-01-10 12:18:48 +0100

    Major new update

--- api.py
+++ api.py
@@ -7,6 +7,7 @@ import traceback
 from tcpip_fp_logging import log
 from dune_client import incr
 from urllib.parse import urlparse, parse_qs
+from tcp_fingerprint_utils import makeOsGuess
 
 API_KEY = os.environ.get('API_KEY', '')
 if not API_KEY:
@@ -21,9 +22,9 @@ def S(string):
         return float(matches[0])
 
 
-class MyServer(BaseHTTPRequestHandler):
-    def __init__(self, data):
-        self.data = data
+class ZardaxtApiServer(BaseHTTPRequestHandler):
+    def __init__(self, fingerprints):
+        self.fingerprints = fingerprints
 
     def __call__(self, *args, **kwargs):
         """ Handle a request """
@@ -53,64 +54,63 @@ class MyServer(BaseHTTPRequestHandler):
     # fingerprint and detect if there is a lie
     def detect_os_mismatch(self, tcp_ip_fp):
         user_agent = self.get_user_agent()
-        # get os by tcp ip fingerprint
-        # Linux, macOS or Windows
-        tcpip_os = {
-            'linux': max(S(tcp_ip_fp["avgScoreOsClass"]['Android']), S(tcp_ip_fp["avgScoreOsClass"]['Linux'])),
-            'win': S(tcp_ip_fp["avgScoreOsClass"]['Windows']),
-            'mac': max(S(tcp_ip_fp["avgScoreOsClass"]['iOS']), S(tcp_ip_fp["avgScoreOsClass"]['macOS'])),
-        }
-        # get highest OS from TCP/IP fingerprint
-        highestOS = max(tcpip_os, key=tcpip_os.get)
+        if user_agent:
+            # get os by tcp ip fingerprint
+            # Linux, macOS or Windows
+            tcpip_os = {
+                'linux': max(S(tcp_ip_fp["avg_score_os_class"]['Android']), S(tcp_ip_fp["avg_score_os_class"]['Linux'])),
+                'win': S(tcp_ip_fp["avg_score_os_class"]['Windows']),
+                'mac': max(S(tcp_ip_fp["avg_score_os_class"]['iOS']), S(tcp_ip_fp["avg_score_os_class"]['macOS'])),
+            }
+            # get highest OS from TCP/IP fingerprint
+            highestOS = max(tcpip_os, key=tcpip_os.get)
 
-        userAgentOS = 'win'
-        if 'Linux' in user_agent or 'Android' in user_agent:
-            userAgentOS = 'linux'
-        if 'Mac OS' in user_agent or 'iPhone' in user_agent:
-            userAgentOS = 'mac'
+            userAgentOS = 'win'
+            if 'Linux' in user_agent or 'Android' in user_agent:
+                userAgentOS = 'linux'
+            if 'Mac OS' in user_agent or 'iPhone' in user_agent:
+                userAgentOS = 'mac'
 
-        return highestOS != userAgentOS
-
-    def handle_authenticated_lookup(self, ip):
-        lookup_ip = self.get_query_arg('ip')
-        if lookup_ip:
-            log('Api Key provided. Looking up IP {}'.format(lookup_ip), 'api')
-            res = self.data.get(lookup_ip, None)
-            if res:
-                res['ip'] = ip
-                res['lookup_ip'] = lookup_ip
-                self.wfile.write(
-                    bytes(json.dumps(res, indent=2, sort_keys=True), "utf-8"))
-            else:
-                msg = {
-                    'lookup_ip': lookup_ip,
-                    'msg': 'no fingerprint for this IP ({} db entries)'.format(len(self.data)),
-                }
-                self.wfile.write(
-                    bytes(json.dumps(msg, indent=2, sort_keys=True), "utf-8"))
+            return highestOS != userAgentOS
         else:
-            self.wfile.write(
-                bytes(json.dumps(self.data, indent=2, sort_keys=True), "utf-8"))
+            return None
 
-    def handle_lookup_by_client_ip(self, ip):
-        log('No Api Key provided. Looking up client IP {}'.format(ip), 'api')
-        res = self.data.get(ip, None)
-        if res:
-            res['ip'] = ip
-            res['os_mismatch'] = self.detect_os_mismatch(res)
+    def handle_lookup(self, client_ip, lookup_ip):
+        fp_list = self.fingerprints.get(lookup_ip, None)
+        if fp_list and len(fp_list) > 0:
+            # return the newest fingerprint
+            fp_res = fp_list[-1]
+            classification = makeOsGuess(fp_res)
+            classification['result']['lookup_ip'] = lookup_ip
+            classification['result']['client_ip'] = client_ip
+            classification['result']['os_mismatch'] = self.detect_os_mismatch(classification)
             self.wfile.write(
-                bytes(json.dumps(res, indent=2, sort_keys=True), "utf-8"))
+                bytes(json.dumps(classification, indent=2, sort_keys=True), "utf-8"))
         else:
             msg = {
-                'ip': ip,
-                'msg': 'no fingerprint for this IP ({} db entries)'.format(len(self.data)),
+                'lookup_ip': lookup_ip,
+                'msg': 'no fingerprint for this IP ({} db entries)'.format(len(self.fingerprints)),
             }
+            log(msg, 'api')
             self.wfile.write(
                 bytes(json.dumps(msg, indent=2, sort_keys=True), "utf-8"))
 
+    def handle_authenticated_lookup(self, client_ip):
+        lookup_ip = self.get_query_arg('ip')
+        if lookup_ip:
+            log('Api Key provided. Looking up IP {}'.format(lookup_ip), 'api')
+            self.handle_lookup(client_ip, lookup_ip)
+        else:
+            self.wfile.write(
+                bytes(json.dumps(self.fingerprints, indent=2, sort_keys=True), "utf-8"))
+
+    def handle_lookup_by_client_ip(self, client_ip):
+        log('No Api Key provided. Looking up client IP {}'.format(client_ip), 'api')
+        self.handle_lookup(client_ip, client_ip)
+
     def do_GET(self):
-        ip = self.get_ip()
-        incr('tcp_ip_fingerprint_public', ip)
+        client_ip = self.get_ip()
+        incr('tcp_ip_fingerprint_public', client_ip)
 
         try:
             if self.path.startswith('/classify'):
@@ -118,12 +118,12 @@ class MyServer(BaseHTTPRequestHandler):
                 self.send_header("Content-type", "text/json")
                 self.end_headers()
                 log('Incoming API request from IP: {} with path: {}'.format(
-                    ip, self.path), 'api')
+                    client_ip, self.path), 'api')
                 key = self.get_query_arg('key')
                 if key and API_KEY == key:
-                    self.handle_authenticated_lookup(ip)
+                    self.handle_authenticated_lookup(client_ip)
                 else:
-                    self.handle_lookup_by_client_ip(ip)
+                    self.handle_lookup_by_client_ip(client_ip)
             else:
                 self.send_response(403)
                 self.end_headers()
@@ -135,9 +135,9 @@ class MyServer(BaseHTTPRequestHandler):
             log(msg, 'api', level='ERROR')
 
 
-def create_server(data):
+def create_server(fingerprints):
     server_address = ('0.0.0.0', 8249)
-    handler = MyServer(data)
+    handler = ZardaxtApiServer(fingerprints)
     httpd = HTTPServer(server_address, handler)
     log("TCP/IP Fingerprint API started on http://%s:%s" %
         server_address, 'api', level='INFO')
@@ -151,6 +151,6 @@ def create_server(data):
     log("TCP/IP Fingerprint API stopped.", 'api', level='INFO')
 
 
-def run_api(data):
-    thread = _thread.start_new_thread(create_server, (data, ))
+def run_api(fingerprints):
+    thread = _thread.start_new_thread(create_server, (fingerprints, ))
     return thread
---
+++ tcp_fingerprint_utils.py
@@ -0,0 +1,124 @@
+import json
+from tcpip_fp_logging import log
+
+databaseFile = './database/January2023Cleaned.json'
+dbList = []
+
+# load fingerprints into database
+with open(databaseFile) as f:
+    dbList = json.load(f)
+log('Loaded {} fingerprints from the database'.format(
+    len(dbList)), 'tcp_fingerprint')
+
+def computeNearTTL(ip_ttl):
+    guessed_ttl_start = ip_ttl
+
+    if ip_ttl > 0 and ip_ttl <= 16:
+        guessed_ttl_start = 16
+    elif ip_ttl > 16 and ip_ttl <= 32:
+        guessed_ttl_start = 32
+    elif ip_ttl > 32 and ip_ttl <= 60:
+        guessed_ttl_start = 60  # unlikely to find many of these anymore
+    elif ip_ttl > 60 and ip_ttl <= 64:
+        guessed_ttl_start = 64
+    elif ip_ttl > 64 and ip_ttl <= 128:
+        guessed_ttl_start = 128
+    elif ip_ttl > 128:
+        guessed_ttl_start = 255
+
+    return guessed_ttl_start
+
+
+def makeOsGuess(fp, n=3):
+    """
+    Return the highest scoring TCP/IP fingerprinting match from the database.
+    If there is more than one highest scoring match, return all the highest scoring matches.
+
+    As a second guess, output the operating system with the highest, normalized average score.
+    """
+    perfectScore = 11.5
+    scores = []
+    for i, entry in enumerate(dbList):
+        score = 0
+        # @TODO: consider `ip_tll`
+        if computeNearTTL(entry['ip_ttl']) == computeNearTTL(fp['ip_ttl']):
+            score += 1.5
+        # @TODO: consider `tcp_window_scaling`
+        # check IP DF bit
+        if entry['ip_df'] == fp['ip_df']:
+            score += 1
+        # check IP MF bit
+        if entry['ip_mf'] == fp['ip_mf']:
+            score += 1
+        # check TCP window size
+        if entry['tcp_window_size'] == fp['tcp_window_size']:
+            score += 1.5
+        # check TCP flags
+        if entry['tcp_flags'] == fp['tcp_flags']:
+            score += 1
+        # check TCP header length
+        if entry['tcp_header_length'] == fp['tcp_header_length']:
+            score += 1
+        # check TCP MSS
+        if entry['tcp_mss'] == fp['tcp_mss']:
+            score += 1.5
+        # check TCP options
+        if entry['tcp_options'] == fp['tcp_options']:
+            score += 3
+        else:
+            # check order of TCP options (this is weaker than TCP options equality)
+            orderEntry = ''.join(
+                [e[0] for e in entry['tcp_options'].split(',') if e])
+            orderFp = ''.join([e[0]
+                              for e in fp['tcp_options'].split(',') if e])
+            if orderEntry == orderFp:
+                score += 2
+
+        scores.append({
+            'i': i,
+            'score': score,
+            'os': entry.get('os', {}).get('name'),
+        })
+
+    # Return the highest scoring TCP/IP fingerprinting match
+    scores.sort(key=lambda x: x['score'], reverse=True)
+    guesses = []
+    highest_score = scores[0].get('score')
+    for guess in scores:
+        if guess['score'] != highest_score:
+            break
+        guesses.append({
+            'score': '{}/{}'.format(guess['score'], perfectScore),
+            'os': guess['os'],
+        })
+
+    # get the os with the highest, normalized average score
+    os_score = {}
+    for guess in scores:
+        if guess['os']:
+            if not os_score.get(guess['os']):
+                os_score[guess['os']] = []
+            os_score[guess['os']].append(guess['score'])
+
+    highest_os_avg = 0
+    highest_os = None
+    avg_os_score = {}
+    for key in os_score:
+        N = len(os_score[key])
+        # only consider OS classes with at least 8 elements
+        if N >= 8:
+            avg = sum(os_score[key]) / N
+            avg_os_score[key] = 'avg={}, N={}'.format(round(avg, 2), N)
+            if avg >= highest_os_avg:
+                highest_os = key
+            highest_os_avg = max(avg, highest_os_avg)
+
+    return {
+        'perfect_score': perfectScore,
+        'best_n_guesses': guesses[:n],
+        'avg_score_os_class': avg_os_score,
+        'fp': fp,
+        'result': {
+          'os_highest_class': highest_os,
+        }
+    }
