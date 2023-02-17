e677733e6ac47e53eff98bfb99f8ad6ea38892a9
Nikolai Tschacher <admin@incolumitas.com>
2023-02-17 11:48:51 +0100

    change format and indentation to 2 spaces

--- zardaxt_logging.py
+++ zardaxt_logging.py
@@ -2,11 +2,11 @@
 from datetime import datetime
 
 def log(msg, module, onlyPrint=False, level='INFO'):
-    msg = f'[{datetime.now()}] - {level} - {module} - {msg}'
-    print(msg)
-    if onlyPrint is False:
-      with open('log/zardaxt.log', 'a') as logfile:
-          logfile.write(msg + '\n')
-      if level == 'ERROR':
-        with open('log/zardaxt.err', 'a') as logfile:
-            logfile.write(msg + '\n')
+  msg = f'[{datetime.now()}] - {level} - {module} - {msg}'
+  print(msg)
+  if onlyPrint is False:
+    with open('log/zardaxt.log', 'a') as logfile:
+      logfile.write(msg + '\n')
+    if level == 'ERROR':
+      with open('log/zardaxt.err', 'a') as logfile:
+        logfile.write(msg + '\n')
